13f429860563a0925e30299db483237b8eb1a3c6
Abhishek Bansal <64872568+abhishek593@users.noreply.github.com>
2021-10-01 19:04:12 +0530

    Updated strongly_connected_components.cpp
    Added some explanation of how the algorithm works, and what other helpful functions are present.

--- C++/Data Structures/Trees/Generic Trees/strongly_connected_components.cpp
+++ C++/Data Structures/Trees/Generic Trees/strongly_connected_components.cpp
@@ -8,6 +8,7 @@ struct SCC{
         timer = 0, numSCC = 0;
         tin.assign(n, -1), low.assign(n, -1), id.resize(n);
     }
+    //Uses dfs low-linking to build the strongly connected components
     void dfs(int i, const vector<vector<int>> &g) {
         s.push(i);
         tin[i] = low[i] = timer++;
@@ -28,15 +29,21 @@ struct SCC{
             numSCC++;
         }
     }
+    //Call this function to build the strongly connected components
     void createStronglyConnectedComponents(vector<vector<int>> &g) {
+        //Standard depth first search to find the components
         for (int i = 0; i < n; ++i) {
             if (tin[i] == -1) {
                 dfs(i, g);
             }
         }
     }
+    //This function returns the strongly connected components, with component 
+    //belonging to same ID in a single vector
     vector<vector<int>> getStronglyConnectedComponents() {
+        //numSCC is the count of number of strongly connected components
         vector<vector<int>> comps(numSCC);
+        //So we iterate over all vertices and add it to the id[vertex] vector.
         for (int i = 0; i < n; ++i) {
             comps[id[i]].push_back(i);
         }
@@ -45,6 +52,8 @@ struct SCC{
     // Creates the meta graph of SCC in 0-based indexing
     vector<vector<int>> createMetaGraph(const vector<vector<int>> &g) {
         vector<vector<int>> meta(numSCC);
+        //We will iterate over all edges and if they don't belong to same component
+        //i.e, their ID'S are different, then we will add and edge between them in meta graph
         for (int i = 0; i < n; ++i) {
             for (int j: g[i]) {
                 if (id[i] != id[j]) {
